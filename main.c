987dca5eaba511fd225bf2509faa213401d8b921
Cloud User <ec2-user@ip-172-31-47-123.ap-south-1.compute.internal>
2026-01-29 13:49:53 +0000

    Fixed C complilation error

--- main.c
+++ main.c
@@ -1,7 +1,40 @@
+#include <stdio.h>
 
-main()
+void biggest3(void);
+void factorial(void);
+
+int main()
 {
-	biggest3();
-	factorial();
+    biggest3();
+    factorial();
+    return 0;
 }
+void biggest3(void)
+{
+    int a = 10, b = 20, c = 15;
+    int max = a;
+
+    if (b > max)
+        max = b;
+    if (c > max)
+        max = c;
+
+    printf("Biggest of three numbers: %d\n", max);
+}
+
+void factorial(void)
+{
+    int n = 5, fact = 1;
+
+    for (int i = 1; i <= n; i++)
+        fact *= i;
+
+    printf("Factorial of %d is %d\n", n, fact);
+}
+
+//main()
+//{
+//	biggest3();
+//	factorial();
+//}
 
--- makefile
+++ makefile
@@ -1,10 +1,22 @@
-ABC.exe:main.o big3.o fact.o
-	gcc -o ABC.exe main.o big3.o fact.o
+# ABC.exe: main.o big3.o fact.o
+# gcc -o ABC.exe main.o big3.o fact.o
+# main.o: main.c
+# gcc -c main.c
+# big3.o: big3.c
+# gcc -c big3.c
+# fact.o: fact.c
+# gcc -c fact.c
 
-main.o:main.c
-	gcc -c main.c
-big3.o:big3.c
-	gcc -c big3.c
-fact.o:fact.c
-	gcc -c fact.c
+CC=gcc
+CFLAGS=-Wall
 
+all: main
+
+main: main.o
+	$(CC) main.o -o main
+
+main.o: main.c
+	$(CC) $(CFLAGS) -c main.c
+
+clean:
+	rm -f *.o main
